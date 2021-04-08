46ce73871c031f1eac8a928eb5b038fe0a43a3e4
Stewart ilondanga <stilondanga@gmail.com>
2021-04-08 12:36:27 +0300

    js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -19,50 +19,50 @@ basicPlan.addEventListener("click", function() {
       standardPlan.classList.add("addOpacity");
       premiumPlan.classList.add("addOpacity");
 
-      /*column_one.forEach(function(column) {
-    column.classList.remove("inactive");
-  });
-  column_two.forEach(function(column) {
-    column.classList.add("inactive");
-  });
+      column_one.forEach(function(column) {
+        column.classList.remove("inactive");
+      });
+      column_two.forEach(function(column) {
+        column.classList.add("inactive");
+      });
 
-  column_three.forEach(function(column) {
-    column.classList.add("inactive");
-  });
-});
+      /*  column_three.forEach(function(column) {
+          column.classList.add("inactive");
+        });
+      });
 
-standardPlan.addEventListener("click", function() {
-  basicPlan.classList.add("addOpacity");
-  standardPlan.classList.remove("addOpacity");
-  premiumPlan.classList.add("addOpacity");
+      standardPlan.addEventListener("click", function() {
+        basicPlan.classList.add("addOpacity");
+        standardPlan.classList.remove("addOpacity");
+        premiumPlan.classList.add("addOpacity");
 
-  column_one.forEach(function(column) {
-    column.classList.add("inactive");
-  });
-  column_two.forEach(function(column) {
-    column.classList.remove("inactive");
-  });
+        column_one.forEach(function(column) {
+          column.classList.add("inactive");
+        });
+        column_two.forEach(function(column) {
+          column.classList.remove("inactive");
+        });
 
-  column_three.forEach(function(column) {
-    column.classList.add("inactive");
-  });
-});
+        column_three.forEach(function(column) {
+          column.classList.add("inactive");
+        });
+      });
 
-premiumPlan.addEventListener("click", function() {
-  basicPlan.classList.add("addOpacity");
-  standardPlan.classList.add("addOpacity");
-  premiumPlan.classList.remove("addOpacity");
+      premiumPlan.addEventListener("click", function() {
+        basicPlan.classList.add("addOpacity");
+        standardPlan.classList.add("addOpacity");
+        premiumPlan.classList.remove("addOpacity");
 
-  column_one.forEach(function(column) {
-    column.classList.add("inactive");
-  });
-  column_two.forEach(function(column) {
-    column.classList.add("inactive");
-  });
+        column_one.forEach(function(column) {
+          column.classList.add("inactive");
+        });
+        column_two.forEach(function(column) {
+          column.classList.add("inactive");
+        });
 
-  column_three.forEach(function(column) {
-    column.classList.remove("inactive");
-  });
-});
+        column_three.forEach(function(column) {
+          column.classList.remove("inactive");
+        });
+      });
 
-basicPlan.click();
+      basicPlan.click();
